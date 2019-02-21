c568b8f0183e7aeabd062d6a05124601f76f14ec
firelice <firelice360@gmail.com>
2019-02-21 16:19:57 -0500

    more jokes!

--- src/fetchlists/fetchlists.py
+++ src/fetchlists/fetchlists.py
@@ -10,6 +10,11 @@ def joke():
         "When does the Ninja arrive at a party...\n Raiton time!",
         "Esuna Matata! What a wonderful phrase!",
         "Why are black mages bad team players... \n Because after thirty seconds they foul everything!",
-        "What do you call a guy playing a female dragoon... \n A DRG Queen!"
+        "What do you call a guy playing a female dragoon... \n A DRG Queen!",
+        "Why did the Dragoon get fired...\n Because they kept asking for a raise!",
+        "What happened when the waiter tripped in Eorzea Cafè...\n The server went down!",
+        "Roegadyn Ninja!",
+        "Vyn's Subway Sandwiches!",
+        "Pineapple on Pizza!"
     ]
     return random.choice(jokes)
