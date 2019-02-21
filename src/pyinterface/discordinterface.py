7f163163de68335925fdbbcab281423ae92f1c0a
firelice <firelice360@gmail.com>
2019-02-21 15:40:58 -0500

    more jokes

--- src/pyinterface/discordinterface.py
+++ src/pyinterface/discordinterface.py
@@ -22,10 +22,15 @@ async def joke(context):
     jokes = [
         "Samurai is a \U0000267F",
         "\U0000267FCactuar  Crew  coming through!\U0000267F",
-        "A dragoon walks into a bar...\n ...Everyone else dodges it.",
+        "A dragoon walks into a bar...\n Everyone else dodges it.",
         "Summoners ruin everything!",
-        "When does the Ninja arrive at a party...\n Raiton time!"
+        "When does the Ninja arrive at a party...\n Raiton time!",
+        "Esuna Matata! What a wonderful phrase!",
+        "Why are black mages bad team players... \n Because after thirty seconds they foul everything.",
+        "What do you call a guy playing a female dragoon... \n A DRG Queen"
     ]
-    await bot.say(random.choice(jokes) + ", " + context.message.author.mention)
+    # The list of jokes
+    await bot.say(context.message.author.mention + "\n" + random.choice(jokes))
+
 
 bot.run(TOKEN)
