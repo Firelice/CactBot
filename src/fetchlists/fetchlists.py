5f9c62a3e5a3fa4030a66f9f9347436f85d7a1f5
Jacob Ortmann <firelice360@gmail.com>
2019-02-21 17:57:58 -0500

    formatting

--- src/fetchlists/fetchlists.py
+++ src/fetchlists/fetchlists.py
@@ -4,15 +4,15 @@ import random
 def joke():
     jokes = [
         "Samurai is a \U0000267F",
-        "\U0000267FCactuar  Crew  coming through!\U0000267F",
-        "A dragoon walks into a bar...\n Everyone else dodges it!",
+        "\U0000267FCactuar Crew coming through!\U0000267F",
+        "A dragoon walks into a bar...\nEveryone else dodges it!",
         "Summoners ruin everything!",
-        "When does the Ninja arrive at a party...\n Raiton time!",
+        "When does the Ninja arrive at a party...\nRaiton time!",
         "Esuna Matata! What a wonderful phrase!",
-        "Why are black mages bad team players... \n Because after thirty seconds they foul everything!",
-        "What do you call a guy playing a female dragoon... \n A DRG Queen!",
-        "Why did the Dragoon get fired...\n Because they kept asking for a raise!",
-        "What happened when the waiter tripped in Eorzea Cafè...\n The server went down!",
+        "Why are black mages bad team players... \nBecause after thirty seconds they foul everything!",
+        "What do you call a guy playing a female dragoon... \nA DRG Queen!",
+        "Why did the Dragoon get fired...\nBecause they kept asking for a raise!",
+        "What happened when the waiter tripped in Eorzea Cafè...\nThe server went down!",
         "Roegadyn Ninja!",
         "Vyn's Subway Sandwiches!",
         "Pineapple on Pizza!"
